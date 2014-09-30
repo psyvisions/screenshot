6ae6a6d3119c8ccad4901e2c755ba620d6a6bbe4
Roald Ignacio <me@apple.mac>
2014-09-30 19:58:25 +0800

    adjust content

--- screenshot.js
+++ screenshot.js
@@ -6,30 +6,32 @@ var args = system.args;
 //see api here: http://phantomjs.org/api/webpage/
 page.viewportSize = {
   width: 810,
-  height: 1000
+  height: 2000
 };
 
 try {
-	page.open(args[1], function(){
-		var url = args[1];
-		var patt = /[0-9]/g;	
-		var pk = url.match(patt).join('');
+    page.open(args[1], function(){
+        // COI Templates
+        var url = args[1];
+        var patt = /[0-9]/g;
+        var pk = url.match(patt).join('');
 
-		page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
-		    page.evaluate(function() {
-		      //disable scrollbars	
-		      $('.scrollableArea').css({
-		      	overflow: "hidden",
-				width: "102%",
-		      });
-		    });
+        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+            page.evaluate(function() {
+              //Disable scrollbars  
+              $('.scrollableArea').css({
+                overflow: "hidden",
+                width: "101%",
+              });
+            });
 
 
-		    page.render('img/screen-'+ pk +'.jpg', {format: 'jpeg', quality: '90'});
-			console.log();
-			phantom.exit();
-		});
-	});
+            page.render('img/screen-'+ pk +'.jpg', {format: 'jpeg', quality: '90'});
+            phantom.exit();
+        });
+    
+
+    });
 }
 catch(err) {
     console.log(err);
